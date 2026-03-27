9ad2b98e9184beb16fddcfcd025c09509939f6c8
Dorotel <149977176+Dorotel@users.noreply.github.com>
2026-03-27 09:20:58 -0500

    fix: correct date filtering in sp_Reporting_Availability_GetByDateRange procedure

--- Database/Database_Deployment/Sql_Files/StoredProcedures/Reporting/sp_Reporting_Availability_GetByDateRange.sql
+++ Database/Database_Deployment/Sql_Files/StoredProcedures/Reporting/sp_Reporting_Availability_GetByDateRange.sql
@@ -8,9 +8,9 @@ CREATE PROCEDURE `sp_Reporting_Availability_GetByDateRange`(
 )
 BEGIN
     SELECT
-        (SELECT COUNT(*) FROM view_receiving_history WHERE created_date BETWEEN p_start_date AND p_end_date) AS receiving_count,
+        (SELECT COUNT(*) FROM view_receiving_history WHERE DATE(created_at) BETWEEN p_start_date AND p_end_date) AS receiving_count,
         (SELECT COUNT(*) FROM view_dunnage_history WHERE created_date BETWEEN p_start_date AND p_end_date) AS dunnage_count,
-    (SELECT COUNT(*) FROM view_volvo_history WHERE DATE(created_date) BETWEEN p_start_date AND p_end_date) AS volvo_count;
+        (SELECT COUNT(*) FROM view_volvo_history WHERE DATE(created_date) BETWEEN p_start_date AND p_end_date) AS volvo_count;
 END $$
 
 DELIMITER ;
